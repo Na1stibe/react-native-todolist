70a29d4bf8d2ca3f3f444507dffeeab3639e8751
na1stone <fheoa700000@naver.com>
2020-05-11 04:57:47 +0900

    newToDostate and input style and scrollview add

--- App.js
+++ App.js
@@ -1,19 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput,Dimensions
-,Platform } from 'react-native';
+import { 
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+  Platform,
+  ScrollView } from 'react-native';
+import ToDo from "./ToDo";
+
 
 const {width,height} = Dimensions.get("window");
 export default class App extends React.Component{
+  state = {
+    newToDo:""
+  };
   render() {
+    const {newToDo} = this.state;
   return (
     <View style={styles.container}>
       <StatusBar barStyle="light-content"/>
       <Text style={styles.title}> Won Seok to do</Text>
      <View style={styles.card}>
-       <TextInput style={styles.input} placeholder={"New To Do"}/>
+       <TextInput style={styles.input} 
+       placeholder={"New To Do"} 
+       value={newToDo}
+       onChangeText={this.plusNewToDo}
+       placeholderTextColor={"#999"}/>
+       <ScrollView>
+         <ToDo/>
+       </ScrollView>
      </View>
     </View>
   );
+ }
+ plusNewToDo = text => {
+   this.setState({
+     newToDo: text
+   });
  }
 }
 
@@ -50,5 +75,11 @@ const styles = StyleSheet.create({
         elevation: 3
       }
     })
+  },
+  input:{
+    padding: 20,
+    borderBottomColor: "#808080",
+    borderBottomWidth: 1,
+    fontSize: 25
   }
 });
---
+++ ToDo.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import {View,Text,TouchableOpacity, StyleSheet} from "react-native";
+//TouchableOpacity = rn touch event view
+// class component 수정하기 위해 사용
+
+export default class ToDo extends React.Component{
+    render(){
+        return(
+        <View>
+            <Text>hi</Text>
+        </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({})
