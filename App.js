7ccfd45bec00a27ff0813a6e4e46aebc2086e2ad
na1stone <fheoa700000@naver.com>
2020-05-11 03:50:30 +0900

    card update

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput,Dimensions
+,Platform } from 'react-native';
 
-
+const {width,height} = Dimensions.get("window");
 export default class App extends React.Component{
   render() {
   return (
     <View style={styles.container}>
-      <Statusbar barStyle/>
+      <StatusBar barStyle="light-content"/>
+      <Text style={styles.title}> Won Seok to do</Text>
+     <View style={styles.card}>
+       <TextInput style={styles.input} placeholder={"New To Do"}/>
+     </View>
     </View>
   );
  }
@@ -15,8 +20,35 @@ export default class App extends React.Component{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#0000FF',
+    backgroundColor: '#ff7f00',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  title:{
+    color: "#808080",
+    fontSize: 30,
+    marginTop: 50,
+    fontWeight:"200",
+    marginBottom: 30
+  },
+  card:{
+    backgroundColor:"white",
+    flex: 1,
+    width: width-50,  
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50,50,50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  }
 });
